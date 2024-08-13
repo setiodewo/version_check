69a9c64209df28360daaf3e736b53f94398e3908
Emanuel Setio Dewo <setiodewo@users.noreply.github.com>
2024-08-13 08:51:07 +0700

    change && to ;
    change && to ;

--- version_check.sh
+++ version_check.sh
@@ -1,14 +1,14 @@
-printf "[HOSTNAME]\n" && \
-hostname 2>/dev/null && \
-printf "\n[OS]\n" && \
-lsb_release -a 2>/dev/null && \
-printf "\n[APACHE]\n" && \
-apache2 -v 2>/dev/null && \
-printf "\n[PHP]\n" && \
-php -v 2>/dev/null && \
-printf "\n[MYSQL]\n" && \
-mysql --version 2>/dev/null && \
-printf "\n[NODE]\n" && \
-node --version 2>/dev/null && \
-printf "\n[PM2]\n" && \
+printf "[HOSTNAME]\n" ; \
+hostname 2>/dev/null ; \
+printf "\n[OS]\n" ; \
+lsb_release -a 2>/dev/null ; \
+printf "\n[APACHE]\n" ; \
+apache2 -v 2>/dev/null ; \
+printf "\n[PHP]\n" ; \
+php -v 2>/dev/null ; \
+printf "\n[MYSQL]\n" ; \
+mysql --version 2>/dev/null ; \
+printf "\n[NODE]\n" ; \
+node --version 2>/dev/null ; \
+printf "\n[PM2]\n" ; \
 pm2 --version 2>/dev/null
